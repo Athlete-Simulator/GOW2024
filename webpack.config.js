fb08dc4c670064c3ca8c3dfcce243e7d0bccc32b
WorKa Adem <adem.bj@gmail.com>
2024-05-31 03:31:49 +0200

    prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const distFolder = "./dist";
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/app.ts',
     plugins: [
         new CleanWebpackPlugin(),
@@ -19,22 +17,8 @@ module.exports = {
                 { from: './sprites', to: 'sprites' },
                 { from: './sounds', to: 'sounds' }
             ]
-        }),
-        new webpack.ProvidePlugin({
-            Ammo: 'ammo.js' // Ensure Ammo is available globally
         })
     ],
-
-    devtool: 'inline-source-map',
-    devServer: {
-        https: {
-            key: path.resolve(__dirname, 'key.pem'),
-            cert: path.resolve(__dirname, 'cert.pem'),
-        },
-        historyApiFallback: true, // Add this if you're dealing with routing
-        port: 8080, // Specify the port (if needed)
-        open: true // Opens the browser when the server starts
-    },
     module: {
         rules: [
             {
@@ -44,43 +28,27 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader', // Creates `style` nodes from JS strings
-                    'css-loader' // Translates CSS into CommonJS
-                ]
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource', // Add this rule if you need to handle images
+                type: 'asset/resource'
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource', // For handling fonts
+                type: 'asset/resource'
             }
         ]
     },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all"
-                }
-            }
-        }
-    },
     resolve: {
-        extensions: [".tsx", ".ts", ".js"],
+        extensions: ['.tsx', '.ts', '.js'],
         fallback: {
-            "fs": false,
             "path": require.resolve("path-browserify")
         }
     },
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, distFolder),
-        clean: true, // Clean the output directory before emit
-        publicPath: '/' // Important for deep linking
+        filename: '[name].[contenthash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     }
 };
